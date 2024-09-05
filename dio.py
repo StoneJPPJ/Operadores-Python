f3e5f0220eff05fbe47b665c405d5d6c514e1c04
João Pedro <129698339+StoneJPPJ@users.noreply.github.com>
2024-09-04 22:35:40 -0300

    Add files via upload

---
+++ dio.py
@@ -0,0 +1,44 @@
+# Operadores Matématicos Simples
+numero1 = 200
+numero2 = 300
+
+numero1 + numero2
+
+numero1 - numero2
+
+numero1 * numero2
+
+numero1 / numero2
+
+
+
+# Operadores Matématicos Complexo
+numero1 // numero2 # Divide e Arredonda para baixo
+
+numero1 ** numero2 # Exponencial
+
+
+
+# Operadores Lógicos
+numero1 and numero2 # Verifica se os dois numeros são iguais, retornando verdadeiro.
+
+numero1 or numero2 # Basta um numero verdadeiro, unico caso de falso é caso todos numeros sejam falsos.
+
+
+
+# Operadores de identificação
+nome = "João"
+
+aluno = nome
+
+aluno is nome # Verifica se eles ocupam o mesmo espaço da mémoria.
+
+aluno is not nome # Verifica se eles não ocupam o mesmo espaço da mémoria.
+
+
+
+# Operadores de associação
+frutas = ["Banana", "Maçã", "Tomate", "Abacaxi"]
+
+"Banana" in frutas  # Verifica se existe o dado dentro da variável
+"Banana" not in  frutas # Verifica se não exite o dado dentro da variável
